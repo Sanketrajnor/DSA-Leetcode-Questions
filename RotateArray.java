d911b0f36defe9dd106d0e1dd30792fc59f925cc
Sanket Rajnor <107184559+Sanketrajnor@users.noreply.github.com>
2024-07-23 15:02:38 +0530

    Create RotateArray.java

---
+++ RotateArray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+    public void reverse(int arr[], int st, int ed){
+        int s = st, e = ed;
+        while(s < e){
+            int temp = arr[s];
+            arr[s] = arr[e];
+            arr[e] = temp;
+            s++;
+            e--;
+        }
+    }
+}
