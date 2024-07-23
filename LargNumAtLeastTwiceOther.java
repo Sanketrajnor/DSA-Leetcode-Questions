8724ccbea9acbc53ea2da33b175ac7de17aa1dd9
Sanket Rajnor <107184559+Sanketrajnor@users.noreply.github.com>
2024-07-23 15:05:19 +0530

    Update LargNumAtLeastTwiceOther.java

--- LargNumAtLeastTwiceOther.java
+++ LargNumAtLeastTwiceOther.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int max = Integer.MIN_VALUE, second = -1, index = -1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > max){
+                second = max;
+                max = nums[i];
+                index = i;
+            }else if(nums[i] > second){
+                second = nums[i];
+            }
+        }
+        return second * 2 <= max ? index : -1;
+    }
+}
